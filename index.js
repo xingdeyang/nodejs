a38eedd30276b660e6dc97f3a1826cb4b8fa3572
dy_xing <xingdeyang@gmail.com>
2020-08-12 14:12:28 +0800

    test jenkins auto ci

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http')
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('helloj enkins')
+    res.end('hello jenkins')
 }).listen(3003)
