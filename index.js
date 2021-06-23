1e35f21ea86bc5e667f3e1f7f3e6d1673e1aca10
dy_xing <dy_xing@yunzhijia.com>
2021-06-23 16:25:25 +0800

    xx

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const http                  = require('http')
 const puppeteer             = require('puppeteer')
+const browserFetcher        = puppeteer.createBrowserFetcher()
 let revisionInfo            = null
 
+browserFetcher.download('843427').then(data => {
+    revisionInfo = data
+})
+
 http.createServer(async function (req, res) {
+    console.log('新请求进入')
     let browser = null
     try {
         browser = await puppeteer.launch({
             headless: true,
             // root用户运行
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            // connect browser超时
+            executablePath: revisionInfo.executablePath
         })
         const page = await browser.newPage()
         await page.setContent('<p style="color: red;">hello world</p>')
@@ -16,7 +24,7 @@ http.createServer(async function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'})
         res.end(content)
     } catch (e) {
-        console.error(e)
+        console.log(e)
         res.writeHead(200, {'Content-Type': 'text/html'})
         res.end('timeout, try later')
     } finally {
