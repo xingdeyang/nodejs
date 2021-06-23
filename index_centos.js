aecc5903b90436bc82a6b5c1196bbc8298054bef
dy_xing <dy_xing@yunzhijia.com>
2021-06-23 16:07:34 +0800

    alpine 要求puppeteer为6.0.0版本

--- index_centos.js
+++ index_centos.js
@@ -1,6 +1,12 @@
+// puppeteer 基于10.0.0, centos 7下运行正常
 const http                  = require('http')
 const puppeteer             = require('puppeteer')
+const browserFetcher        = puppeteer.createBrowserFetcher()
 let revisionInfo            = null
+
+browserFetcher.download('884014').then(data => {
+    revisionInfo = data
+})
 
 http.createServer(async function (req, res) {
     let browser = null
@@ -8,7 +14,9 @@ http.createServer(async function (req, res) {
         browser = await puppeteer.launch({
             headless: true,
             // root用户运行
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            // connect browser超时
+            executablePath: revisionInfo.executablePath
         })
         const page = await browser.newPage()
         await page.setContent('<p style="color: red;">hello world</p>')
