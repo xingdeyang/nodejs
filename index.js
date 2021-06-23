7c4273fbd18083d0a4a82edc6358974fdbe704bf
dy_xing <dy_xing@yunzhijia.com>
2021-06-23 14:03:21 +0800

    xx

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const http      = require('http')
-const puppeteer = require('puppeteer')
+const http                  = require('http')
+const puppeteer             = require('puppeteer')
+const browserFetcher        = puppeteer.createBrowserFetcher()
+let revisionInfo            = null
+
+browserFetcher.download('884014').then(data => {
+    revisionInfo = data
+})
 
 http.createServer(async function (req, res) {
-    const browserFetcher = puppeteer.createBrowserFetcher()
-    const revisionInfo = await browserFetcher.download('884014')
-    const browser = await puppeteer.launch({
-        headless: true,
-        // root用户运行
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        // connect browser超时
-        executablePath: revisionInfo.executablePath
-    })
-    const page = await browser.newPage()
-    await page.setContent('<p style="color: red;">hello world</p>')
-    const content = await page.content()
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end(content)
-    await page.close()
+    let browser = null
+    try {
+        browser = await puppeteer.launch({
+            headless: true,
+            // root用户运行
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            // connect browser超时
+            executablePath: revisionInfo.executablePath
+        })
+        const page = await browser.newPage()
+        await page.setContent('<p style="color: red;">hello world</p>')
+        const content = await page.content()
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end(content)
+    } catch (e) {
+        console.error(e)
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end('timeout, try later')
+    } finally {
+        browser && browser.close()
+    }
 }).listen(3003)
